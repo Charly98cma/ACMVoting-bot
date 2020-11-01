2e75b25b739d10ebd61d362dbb3cf16d0e561165
Charly98cma <1998cma@gmail.com>
2020-11-01 10:24:44 +0100

    First commit

---
+++ msgs.py
@@ -0,0 +1,11 @@
+start_msg = "<b>Bienvenido/a al bot de las elecciones de ACM UPM</b>\n\n" +\
+    +\
+    "Debido a la situación actual, no podremos realizar las elecciones de forma presencial como anteriormente, así que hemos decidido crear este bot para poder registrar los votos de todos los alumnos/as activos en la asociación, es decir, que sigan en la universidad que quieran votar.\n\n" +\
+    +\
+    "Para registrarte en las votaciones, solo tienes que usar el comando <b>/registrarme</b> y se te incluirá en la lista de votantes.\n\n" +\
+    +\
+    "El día de  las votaciones, solo los alumnos/as que se hayan registrado previamente podrán votar. Cabe decir que los <b>votos serán secretos</b>, y que en esta fase de registro solo se usará vuestro nombre y nick de Telegram para verificar que pertenecéis a la asociación antes del día de las votaciones."
+
+user_already_registered = "<b>Ya estabas registrado/a</b> en la lista de participación de las elecciones de ACM UPM"
+
+user_registered = "<b>Se te acaba de registrar</b> en la lista de participación de las elecciones de ACM UPM"
